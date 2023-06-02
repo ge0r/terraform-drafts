da9ff3a5305186e238611ea63743c8009357740c
Georgios Gkioulis <georgios.gkioulis@paessler.com>
2023-06-02 16:29:50 +0200

    Format terraform configuration files

--- example1/variables.tf
+++ example1/variables.tf
@@ -1,30 +1,30 @@
 variable "pet" {
-    default = ["cat", "dog", "parrot"]
-    type = list
+  default = ["cat", "dog", "parrot"]
+  type    = list(any)
 }
 
 variable "file-content" {
-    type = map
-    default = {
-        "cat" = "I love cats"
-        "dog" = "I love dogs"
-    }
+  type = map(any)
+  default = {
+    "cat" = "I love cats"
+    "dog" = "I love dogs"
+  }
 }
 
 variable "Toni" {
-    type = object({
-      name = string
-      color = string
-      age = number
-      food = list(string)
-      favorite_pet = bool
-    })
+  type = object({
+    name         = string
+    color        = string
+    age          = number
+    food         = list(string)
+    favorite_pet = bool
+  })
 
-    default = {
-        name = "Toni"
-        color = "black"
-        age = "2"
-        food = ["chicken", "turkey", "meatball"]
-        favorite_pet = true
-    }
+  default = {
+    name         = "Toni"
+    color        = "black"
+    age          = "2"
+    food         = ["chicken", "turkey", "meatball"]
+    favorite_pet = true
+  }
 }
