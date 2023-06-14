85adc7be868813140788e03be6b458ad46e02977
Georgios Gkioulis <georgios.gkioulis@paessler.com>
2023-06-14 16:33:29 +0200

    Add meta argument notes

--- example1/main.tf
+++ example1/main.tf
@@ -16,10 +16,37 @@ resource "random_pet" "cat" {
   length    = 1
 }
 
-# var.pet from variables.tf file
+# The array var.pet is defined in variables.tf file
 resource "local_file" "pet2" {
   filename = var.pet[0] # filename will be cat
   content  = "Pets2!"
+
+  lifecycle {
+    # Reject any changes that would result into the resource getting destroyed (you could still just terraform destroy)
+    # prevent_destroy = true
+
+    # When updating this resource (actually due to the nature of immutable infrastructure, TF will delete the resource and replace it with a new one)
+    # create the new resource first before destroying the old resource.
+    create_before_destroy = true
+  }
+}
+
+# Loops based on `count` and creates multiple files with names from the name bird-filename variable list.
+# The resource local_file.bird will now be created as a list
+resource "local_file" "bird" {
+  filename = var.bird-filename[count.index]
+  content = "test"
+  count = length(var.bird-filename)
+}
+
+# The for_each meta argument acts similarly to loop. However it needs a variable set (a list without repeating elements) , not a list.
+# We could also use `for_each = toset(var.reptile-filename)` if reptile-filename was a list instead of set
+# The resource local_file.bird will now be created as a map. <---------
+# This is recommended because it maps the resource to the filename as key, and not an index, and it will only delete the appropriate resource if the set changes.
+resource "local_file" "reptile" {
+  filename = each.value
+  content = "test"
+  for_each = var.reptile-filename
 }
 
 # Example Access Resource Attributes
@@ -50,7 +77,19 @@ resource "local_file" "welcome" {
   filename = "/root/message.txt"
   content  = "Welcome to Kodekloud."
 }
+
+# Will output (print) the value of the welcome file resource
 output "welcome_message" {
   value       = local_file.welcome.content
   description = "The content value of the local_file welcome resource"
+}
+
+# Will output (print) the output of /etc/os-release
+output "os-version" {
+  value = data.local_file.os.content
+}
+
+# We treat /etc/os-release file as a data source, so despite not being provisioned by terraform we will read its contents
+data "local_file" "os" {
+  filename = "/etc/os-release"
 }
--- example1/variables.tf
+++ example1/variables.tf
@@ -11,7 +11,7 @@ variable "file-content" {
   }
 }
 
-variable "Toni" {
+variable "toni" {
   type = object({
     name         = string
     color        = string
@@ -27,4 +27,23 @@ variable "Toni" {
     food         = ["chicken", "turkey", "meatball"]
     favorite_pet = true
   }
+}
+
+variable "bird-filename" {
+  type=list(string) # type is not explicitly required
+  default = [
+    "/root/parrot.txt",
+    "/root/sparrow.txt",
+    "/root/eagle.txt",
+    "/root/duck.txt"
+  ]
+}
+
+variable "reptile-filename" {
+  type=set(string) # type is not explicitly required
+  default = [
+    "/root/lizzard.txt",
+    "/root/snake.txt",
+    "/root/turtle.txt",
+  ]
 }
